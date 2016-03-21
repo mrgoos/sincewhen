0d501fd18b58e49a864e1208e2da337e0e3a7140
ofer.mark <ofer.mark@bigpanda.io>
2016-03-22 00:11:51 +0200

    added insertads method

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var mongodb = require('mongodb');
 var express = require('express');
 //var ejs = require('ejs');
 var http = require('http');
+var bodyParser = require('body-parser');
 var env = process.env;
 
 var app = express();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-
 
 app.use(express.static(__dirname + '/public'));
 //app.set('view engine', 'ejs');
@@ -16,39 +19,58 @@ app.get('/getads', function(req, res) {
     if (realIDs && realIDs.length) {
         //avid too large array
         if (realIDs.length < 60) {
-            var cursor = db.collection('yad2').find({ realID: { $in: realIDs } }, { _id: 0 }).sort({ realID: 1, timestamp: -1 });
+            var cursor = yad2Collection.find({ realID: { $in: realIDs } }, { _id: 0 }).sort({ realID: 1, timestamp: -1 });
             cursor.toArray(function(err, ids) {
                 if (err) {
                     console.log('error: Could not fulfill request: ' + err);
                     res.status(500).send({ error: 'Could not fulfill request: ' + err });
                 } else {
                     res.status(200).send(ids);
-                    console.log('ids:', ids);
+                    //console.log('ids:', ids);
                 }
             });
         } else {
             //array length is too large
-            res.send(500, { error: 'Nice try! :)' });
+            res.status(500).send({ error: 'Nice try! :)' });
             console.log('array length is way too large:', realIDs.length);
         }
     } else {
         //realIDs wasn't sent
-        res.send(500, { error: 'Nice try! :)' });
-        console.log('No IDs were sent');
+        res.status(500).send({ error: 'Nice try! :)' });
+        console.log('No IDs were sent. Got: ', realIDs);
     }
 });
+
+app.post('/insertads', function(req, res) {
+    if (Array.isArray(req.body)) {
+          yad2Collection.insert(req.body, function(e, results){
+            if (e) {
+                res.status(500).send({ error: 'What are you trying to insert???' });
+            }
+            res.end(results && results.insertedCount && results.insertedCount.toString());
+            //console.log(results);
+          })
+    } else {
+        //no array set
+        res.status(500).send({ error: 'Nice try! :)' });
+        console.log('No array sent to post. Got: ', req.body);
+    }
+
+})
 
 var serverIp = env.NODE_IP || 'localhost';
 var serverPort = env.NODE_PORT || 3000;
 var dbUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/sincewhen';
 var db;
 var server;
+var yad2Collection;
 mongodb.MongoClient.connect(dbUri, { server: { auto_reconnect: true } }, function(err, database) {
     if (err) {
         console.log('err: ', err);
     }
     else {
         db = database;
+        yad2Collection = db.collection('yad2');
         console.log('connected to DB!');
     }
 });
